9fbcece19066e62e1214751040109aec53f1c5c0
henning phan <github@h.phan.se>
2024-12-22 02:50:38 +0100

    export multiple dirs

--- nixosmodules/nfs.nix
+++ nixosmodules/nfs.nix
@@ -6,8 +6,10 @@
   services.nfs.server = {
     enable = true;
     exports = ''
-      /exports    192.168.1.0/24(anongid=20,rw,insecure,sync,no_subtree_check,fsid=root,crossmnt)
-      /exports/tv-shows    192.168.1.0/24(anongid=20,rw,insecure,sync,no_subtree_check)
+      /exports 192.168.1.0/24(anonuid=1000,anongid=1000,rw,insecure,sync,no_subtree_check,fsid=root,crossmnt,all_squash,mountpoint=/exports/black-masstorage)
+      /exports/black-masstorage 192.168.1.0/24(anonuid=1000,anongid=1000,rw,insecure,sync,no_subtree_check,crossmnt,all_squash)
+      /exports/tv-shows 192.168.1.0/24(anonuid=2003,anongid=10001,rw,insecure,sync,no_subtree_check,crossmnt,all_squash)
+      /exports/movies 192.168.1.0/24(anonuid=2002,anongid=10001,rw,insecure,sync,no_subtree_check,crossmnt,all_squash)
     '';
   };
   networking.firewall.allowedTCPPorts = [
