c5e115dde7860d73d1b912aa1c4e5f44c5424023
Henning phan <tuan.hy.henning.phan@volvocars.com>
2025-01-08 22:19:08 +0100

    add more alias to zsh

--- hmmodules/zsh/default.nix
+++ hmmodules/zsh/default.nix
@@ -7,6 +7,12 @@
   programs.atuin = {
     enable = true;
      enableZshIntegration = true;
+    flags = [
+      "--disable-up-arrow"
+    ];
+    settings = {
+      enter_accept = false;
+    };
   };
 
   programs.zsh = {
@@ -16,18 +22,22 @@
       highlight = "fg=#ff00ff,bg=cyan,bold,underline";
     };
     shellAliases = {
+      amende = "git commit --amend --no-edit";
       amend = "git commit --amend";
       gag = "gau && amende && git review";
+      gpr = "git pull --rebase";
       gau = "git add -u ";
       gd = "git diff";
-      gl =  "git log --name-only";
+      gl =  "git log --name-status";
       gs = "git status";
+      s = "sudo --preserve-env --preserve-env=PATH env";
     };
     initExtra = ''
       startcisco(){
         cd ~/repo/openconnect-sso/ || exit 1
           ./result/bin/openconnect-sso -p VolvoCars-Linux_Profile.xml
       }
+      eval "$(direnv hook zsh)"
       '';
 
     cdpath = [ "~/repo" ];
