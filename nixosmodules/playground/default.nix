fff20ff2ff7458c7e950b841ae3e8ccf1ed8d284
Henning phan <tuan.hy.henning.phan@volvocars.com>
2025-01-28 00:48:16 +0100

    playground module to test macvlan

--- nixosmodules/playground/default.nix
+++ nixosmodules/playground/default.nix
@@ -1,50 +1,55 @@
 { config, pkgs, ... }:
 let
-    lib = pkgs.lib;
+  lib = pkgs.lib;
   interface = "wlp0s20f3";
 in
-  {
+{
   networking.macvlans."mv-${interface}-host" = {
-            interface =interface;
-          mode = "bridge";
-          };
-    networking.interfaces."mv-${interface}-host" = {
-            ipv4.addresses = [ { address = "192.168.1.1"; prefixLength = 24; } ];
-        };
+    interface = interface;
+    mode = "bridge";
+  };
+  networking.interfaces."mv-${interface}-host" = {
+    ipv4.addresses = [
+      {
+        address = "192.168.1.1";
+        prefixLength = 24;
+      }
+    ];
+  };
 
   containers.play = {
-      autoStart = true;
+    autoStart = true;
     # enable plex hardware acceleration
     # persist servarr config and media
     privateNetwork = false;
-      macvlans = [ interface ];
-      config =
-        { config, pkgs, ... }:
-        {
+    macvlans = [ interface ];
+    config =
+      { config, pkgs, ... }:
+      {
 
-          nixpkgs.config.allowUnfree = true;
-          networking.firewall.enable = false;
-          networking.interfaces."mv-${interface}" = {
-            ipv4.addresses = [
-              {
-                address = "192.168.1.13";
-                prefixLength = 24;
-              }
-            ];
-          };
-      services.static-web-server = {
+        nixpkgs.config.allowUnfree = true;
+        networking.firewall.enable = false;
+        networking.interfaces."mv-${interface}" = {
+          ipv4.addresses = [
+            {
+              address = "192.168.1.13";
+              prefixLength = 24;
+            }
+          ];
+        };
+        services.static-web-server = {
           enable = true;
           root = "/tmp";
           listen = "0.0.0.0:8787";
-          };
+        };
 
-      environment.systemPackages = with pkgs; [
-        vim
-        silver-searcher
-      ];
+        environment.systemPackages = with pkgs; [
+          vim
+          silver-searcher
+        ];
 
-      system.stateVersion = "23.11"; # Did you read the comment?
-    };
+        system.stateVersion = "23.11"; # Did you read the comment?
+      };
 
   };
 }
