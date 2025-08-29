6770de10b9c5f491fd4407f8ab795e8b9aec694f
henning phan <henning.phan@devies.se>
2025-08-29 23:44:51 +0200

    clean nvim lsp config and activate new lsp's

--- hmmodules/neovim/nvim/lua/lsp.lua
+++ hmmodules/neovim/nvim/lua/lsp.lua
@@ -1,45 +1,35 @@
-local nvim_lsp = require('lspconfig')
-
 local format_on_close = function()
-  vim.api.nvim_create_autocmd({"BufWritePre"}, {
-      callback = function()vim.lsp.buf.format { async = false }end
+  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    callback = function() vim.lsp.buf.format { async = false } end
   })
 end
 
--- See:
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+-- See link for examples and config:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
 local servers = {
-  lua_ls={
+  --dockerls={},
+  ansiblels = {},
+  bashls = {},
+  gopls = {},
+  helm_ls = {},
+  lua_ls = {
     settings = {
       Lua = {
         diagnostics = {
-	  globals = { 'vim' }
-	}
+          globals = { 'vim' } }
       }
     }
   },
-  helm_ls={},
-  nil_ls={},
-  eslint={},
-  gopls={
-    on_attach=format_on_close
-  },
-  pyright={},
-  bashls={},
-  --dockerls={},
-  clangd={},
-  terraformls={_on_attach=function()
-	  vim.api.nvim_create_autocmd({"BufWritePre"}, {
-	      pattern = {"*.tf", "*.tfvars"},
-	      callback = vim.lsp.buf.formatting_sync
-	  })
-      end
-  },
+  marksman = {},
+  nil_ls = {},
+  pyright = {},
+  terraformls = {},
+  yamlls = {},
 }
 
+vim.lsp.config['*'] = { on_attach = format_on_close }
+
 for lsp, conf in pairs(servers) do
-  conf['flags'] = {
-      debounce_text_changes = 150,
-  }
-  nvim_lsp[lsp].setup(conf)
+  vim.lsp.config[lsp] = conf
+  vim.lsp.enable(lsp)
 end
