4c05284c2c2fef10cd8901e17bca69984d371c0d
Henning phan <tuan.hy.henning.phan@volvocars.com>
2025-09-08 13:13:25 +0200

    remove dead packages

--- hosts/vcc/vcc.hm.nix
+++ hosts/vcc/vcc.hm.nix
@@ -28,43 +28,30 @@
   henning.firefox.enable = true;
   henning.tmux.enable = true;
   henning.tmux.defaultShell = "~/.nix-profile/bin/zsh";
-  home.packages =
-    let
-      vpn = pkgs.writeShellScriptBin "vpn" ''
-        #!/bin/bash
-        set -e
-        export POINTSHARP_TOKEN="$(cloak view vcc)"
-        vccvpn "$@"
-      '';
-    in
-    with pkgs;
-    [
-      brightnessctl
-      cloak
-      comma
-      devenv
-      direnv
-      element-desktop
-      git
-      git-review
-      gnomeExtensions.dash-to-dock
-      gnomeExtensions.dash-to-panel
-      libreoffice
-      nh
-      nix
-      nix-direnv
-      nix-index
-      python311Packages.grip
-      remmina
-      silver-searcher
-      slack
-      spotify
-      teams-for-linux
-      ungoogled-chromium
-      vpn
-      xclip
-      yq
-    ];
+  home.packages = with pkgs; [
+    brightnessctl
+    cloak
+    comma
+    devenv
+    element-desktop
+    git
+    git-review
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.dash-to-panel
+    libreoffice
+    nh
+    nix
+    nix-index
+    python311Packages.grip
+    remmina
+    silver-searcher
+    slack
+    spotify
+    teams-for-linux
+    ungoogled-chromium
+    xclip
+    yq
+  ];
 
   programs.neovim.extraPlugins = with pkgs.vimPlugins; [ copilot-vim ];
   # Let Home Manager install and manage itself.
