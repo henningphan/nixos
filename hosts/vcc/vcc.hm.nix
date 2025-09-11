c08273b7bd8e0268bb2c18a96b406ec4f6ece99a
Henning phan <tuan.hy.henning.phan@volvocars.com>
2025-09-11 16:13:13 +0200

    only keep essential configs in vcc.hm.nix

--- hosts/vcc/vcc.hm.nix
+++ hosts/vcc/vcc.hm.nix
@@ -1,14 +1,5 @@
 { config, pkgs, ... }:
 {
-  dconf = {
-    settings."org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = with pkgs.gnomeExtensions; [
-        dash-to-dock.extensionUuid
-        dash-to-panel.extensionUuid
-      ];
-    };
-  };
   # complement missing
   #home.username = "${cdsid}";
   # complement missing
@@ -29,45 +20,9 @@
   henning.tmux.enable = true;
   henning.tmux.defaultShell = "~/.nix-profile/bin/zsh";
   home.packages = with pkgs; [
-    brightnessctl
-    cloak
-    comma
-    devenv
-    element-desktop
-    git
-    git-review
-    gnomeExtensions.dash-to-dock
-    gnomeExtensions.dash-to-panel
-    libreoffice
-    nh
-    nix
-    nix-index
-    python311Packages.grip
-    remmina
-    silver-searcher
-    slack
-    spotify
-    teams-for-linux
-    ungoogled-chromium
-    xclip
-    yq
   ];
 
   programs.neovim.extraPlugins = with pkgs.vimPlugins; [ copilot-vim ];
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  programs.fzf = {
-    enable = true;
-    enableBashIntegration = true;
-  };
-  programs.git = {
-    enable = true;
-    delta.enable = true;
-    delta.options.side-by-side = true;
-    # complement missing
-    # userEmail = "${email}";
-    userName = "Henning phan";
-    extraConfig.gitreview.remote = "origin";
-  };
-
 }
